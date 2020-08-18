bc73ae2238b9c786d7cec182f350f58accef298d
Samuel Morgan-Tyghe <sa>
2020-08-18 16:33:50 +0100

    blank slate

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Nav from './Nav';
+import About from './About';
+import Projects from './Projects';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 
 function App() {
   return (
+    <Router>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Nav />
+      <switch>
+      <Route path="/" exact component={home} />
+      <Route path="/About" exact component={About} />
+      <Route path="/Projects" exact component={Projects} />
+      </switch>
+      
     </div>
+    </Router>
   );
 }
+
+
+const home = () => (
+  <div>
+    <h1>Home Page</h1>
+  </div>
+);
 
 export default App;
